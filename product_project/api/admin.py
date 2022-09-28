6469e6ba27435517328215df37c3d2d55c87700b
algalyq <algalyq@mail.ru>
2022-09-28 21:55:34 +0600

    insert boss and employees

--- product_project/api/admin.py
+++ product_project/api/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Employee
+from .models import Employee, Boss
 
 @admin.register(Employee)
 class EmployeeModel(admin.ModelAdmin):
     list_display = ['Surename_empl', 'Name_empl', 'Post', 'Salary']
+
+
+@admin.register(Boss)
+class BossModel(admin.ModelAdmin):
+    list_display = ['Surename_Boss', 'Name_Boss', 'Post_Boss']
--- product_project/api/models.py
+++ product_project/api/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
-choices_post = (("MANAGER", "Manager"),
-            ("FINANCE DIRECTOR", "Finance Director"),
-            ("PRODUCT DIRECTOR", "Product Director"),
-            ("IT DIRECTOR", "IT Director"),
-            ("GENERAL DIRECTOR", "General Director"))
+choices_post = (("Finance Manager", "Finance Manager"),
+            ("Product Manager", "Product Manager"),
+            ("IT Manager", "IT Manager"),
+             ("Finance Director", "Finance Director"),
+            ("Product Director", "Product Director"),
+            ("IT Director", "IT Director"))
 
+choices_boss = ( ("Finance Director", "Finance Director"),
+            ("Product Director", "Product Director"),
+            ("IT Director", "IT Director"),
+            ("General Director", "General Director"))
+
+class Boss(models.Model):
+    Surename_Boss = models.CharField(max_length=50)
+    Name_Boss = models.CharField(max_length=50)
+    Post_Boss = models.CharField(choices=choices_boss, max_length=50)
+
+
+    def __str__(self):
+        return '%s : %s' % (self.Name_Boss, self.Post_Boss)
+    
 
 class Employee(models.Model):
     Surename_empl = models.CharField( max_length=50)
@@ -13,4 +28,5 @@ class Employee(models.Model):
     Post = models.CharField(choices=choices_post, max_length=50, default='MANAGER')
     Date_employment = models.DateField(auto_now_add=True)
     Salary = models.PositiveIntegerField(default=0)
+    Boss = models.ForeignKey(Boss, on_delete=models.CASCADE)
 
